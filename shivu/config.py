1c4894ad9d71fc29fdd033acb3d5f13dab0fa79c
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-04 23:26:34 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6352061770"
     sudo_users = "6352061770", "6176582997"
-    GROUP_ID = -1001916618183
-    TOKEN = "7047990843:AAFxAKHcnF5JHCHoy9UG03e612-_aDCXgAs"
+    GROUP_ID = -1002040470360
+    TOKEN = "7159220855:AAF8SMfMDgI3IQU0i1vObY5-mCNu42v_CGY"
     mongo_url = "mongodb+srv://riyu:riyu@cluster0.kduyo99.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "riyadori"
+    SUPPORT_CHAT = "ARI_BOTS"
     UPDATE_CHAT = "animetubeworld"
-    BOT_USERNAME = "gojosatoruriyabot"
-    CHARA_CHANNEL_ID = "-1001916618183"
+    BOT_USERNAME = "doreriyabot"
+    CHARA_CHANNEL_ID = "-1002040470360"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
