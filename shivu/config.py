d6b5ad977fd258bb12ea9cc19d526da10dd8643d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-08-30 19:28:27 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,7 +8,7 @@ class Config(object):
     TOKEN = "7063220368:AAEty7C6Z0MrlmUpmPH81J-AhM7Jyum4oOI"
     mongo_url = "mongodb+srv://GOKU:MISSBHOPALI@goku.pzzsl8d.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL =["https://telegra.ph/file/44abc724240ff3cfc2d84.jpg"]
-    SUPPORT_CHAT = "NAMIKAZECPAN"
+    SUPPORT_CHAT = "ZTX_MAIN_CHATS"
     UPDATE_CHAT = "NAMIKAZECPAN"
     BOT_USERNAME = "GUESSEM_ALL_ROBOT"
     CHARA_CHANNEL_ID = "-1002004197830"
