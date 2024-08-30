ac0dbb782bd22ca416b3327633aba961ff1e7609
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-08-30 19:23:06 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7089408502"
     sudo_users = "7089408502"
-    GROUP_ID = -1002004197830
+    GROUP_ID = -1001529779762
     TOKEN = "7063220368:AAEty7C6Z0MrlmUpmPH81J-AhM7Jyum4oOI"
     mongo_url = "mongodb+srv://tiwarireeta004:peqxLEd36RAg7ors@cluster0.furypd3.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL =["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
